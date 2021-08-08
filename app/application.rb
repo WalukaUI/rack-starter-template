f6766e2d78a46f8ac9d8ed43382b06cec53abcf7
WalukaUI <cwaluka@yahoo.com>
2021-08-07 21:35:17 -0700

    adding /players/id path

--- app/application.rb
+++ app/application.rb
@@ -4,21 +4,26 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/players/) 
+    if req.path.match("/players") && req.get?
       players=Player.all
       return [200, 
       { 'Content-Type' => 'application/json' }, 
       [ players.to_json ]]
-    elsif req.path.match(/teams/) 
+    elsif req.path.match("/teams") && req.get?
       teams=Team.all
       return [200, 
       { 'Content-Type' => 'application/json' }, 
       [ teams.to_json ]]
-    elsif req.path.match(/tournament/) 
+    elsif req.path.match("/tournament") && req.get?
       tournaments=Tournament.all
       return [200, 
       { 'Content-Type' => 'application/json' }, 
       [  tournaments.to_json ]]
+    elsif req.path.match(/players/) && req.get?
+      id = req.path.split('/')[2]
+      player=Players.find_by(id: id)
+      if player
+        
     else
       res.write "Path Not Found"
     end
